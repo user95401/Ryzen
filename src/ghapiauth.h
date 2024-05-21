0c1a3d7976b53b0f4af2190bb6a838f2c7ec10f2
user666 <90561697+user95401@users.noreply.github.com>
2024-05-21 21:57:43 +0300

    Update ghapiauth.h

--- src/ghapiauth.h
+++ src/ghapiauth.h
@@ -5,7 +5,7 @@ inline static matjson::Value AUTH_DATA;
 
 #define ghapiauth \
 .userAgent(AUTH_DATA["name"].as_string()) \
-.header("X-GitHub-Api-Version", "2022-11-28") \
+//.header("X-GitHub-Api-Version", "2022-11-28") \
 //.header("Authorization", fmt::format("Bearer {}", AUTH_DATA["address"].as_string()))
 
 $on_mod(Loaded) {
