a9194e53ddb74281b3a9cc16f233ab6ed525b0de
user95401 <user95401@gmail.com>
2024-05-22 08:22:25 +0400

    i think its danger shit

--- src/ghapiauth.h
+++ src/ghapiauth.h
@@ -1,12 +1,12 @@
 #include <Geode/Geode.hpp>
 using namespace geode::prelude;
 
-inline static matjson::Value AUTH_DATA;
+auto default_token = "Z2hwX3l1aTNPemtNMXhFSDdtS2RwbGpLeG8yRVU2N1dxczM3bUd3Wg==";
 
 #define ghapiauth \
-.userAgent(AUTH_DATA["name"].as_string()) \
-//.header("X-GitHub-Api-Version", "2022-11-28") \
-//.header("Authorization", fmt::format("Bearer {}", AUTH_DATA["address"].as_string()))
+.userAgent(Mod::get()->getID()) \
+.header("X-GitHub-Api-Version", "2022-11-28") \
+.header("Authorization", fmt::format("Bearer {}", ZipUtils::base64URLDecode(default_token)))
 
 $on_mod(Loaded) {
     //tryLoadAuthData();
